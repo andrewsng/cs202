7077fab074a177bab8d1698b7d267e017da511a7
Andrew Ng <ngandrew1337@gmail.com>
2020-01-17 12:11:06 -0900

    implemented cpp_ftoc function using an istringstream

--- hw00/Temperature/temperature.cpp
+++ hw00/Temperature/temperature.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 
 
@@ -11,13 +12,16 @@ using std::vector;
 
 double cpp_ftoc(const char* str)
 {
-
+	double result;
+	std::istringstream istr(str);
+	istr >> result;
+	return (result - 32.0) * (5.0 / 9.0);
 }
 
 
 double c_ctof(const char* str)
 {
-
+	return 0.0;
 }
 
 
