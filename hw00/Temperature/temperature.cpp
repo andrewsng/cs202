0f2d65ddae40ae2bb43a880db07eef0eadd32d34
Andrew Ng <ngandrew1337@gmail.com>
2020-01-17 11:24:48 -0900

    Adding test for --ftoc argument in main

--- hw00/Temperature/temperature.cpp
+++ hw00/Temperature/temperature.cpp
@@ -9,7 +9,24 @@ using std::string;
 using std::vector;
 
 
-int main(int argc, char** argv)
+double cpp_ftoc(const char* str)
 {
 
+}
+
+
+double c_ctof(const char* str)
+{
+
+}
+
+
+int main(int argc, char** argv)
+{
+	if (argc == 4) {
+		if (argv[2] == string("--ftoc")) {
+			double result = cpp_ftoc(argv[3]);
+			cout << result << endl;
+		}
+	}
 }
