1bec701d5fd0682a7b3918eb9754083115696d7c
Andrew Ng <ngandrew1337@gmail.com>
2020-01-18 14:04:23 -0900

    Added error checking for bad input in ctof function

--- hw00/Temperature/temperature.cpp
+++ hw00/Temperature/temperature.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cctype>
 
 
 using std::cout;
@@ -27,24 +28,34 @@ double c_ctof(const char* str)
 {
 	char* err;
 	double result = strtod(str, &err);
-	cout << *err << endl;
+	if (*err != 0) {
+		cout << "ERROR: Could not read temperature value" << endl;
+		return -9999;
+	}
 	return result * (9.0 / 5.0) + 32.0;
 }
 
 
 int main(int argc, char** argv)
 {
-	if (argc == 4) {
-		if (argv[2] == string("--ftoc")) {
-			double result = cpp_ftoc(argv[3]);
+	if (argc >= 3) {
+		if (argv[1] == string("--ftoc")) {
+			double result = cpp_ftoc(argv[2]);
 			if (result == -9999) {
 				return 0;
 			}
 			cout << result << endl;
 		}
-		if (argv[2] == string("--ctof")) {
-			double result = c_ctof(argv[3]);
+		else if (argv[1] == string("--ctof")) {
+			double result = c_ctof(argv[2]);
+			if (result == -9999) {
+				return 0;
+			}
 			cout << result << endl;
+		}
+		else {
+			cout << "ERROR: Argument not found\n"
+				 << "Available functions: --ftoc, --ctof" << endl;
 		}
 	}
 }
