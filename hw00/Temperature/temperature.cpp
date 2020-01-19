97613b378905f2032167b84047e400db5c2585ea
Andrew Ng <ngandrew1337@gmail.com>
2020-01-19 01:25:13 -0900

    Adding comments and moving error messages

--- hw00/Temperature/temperature.cpp
+++ hw00/Temperature/temperature.cpp
@@ -1,3 +1,12 @@
+/*
+Andrew Ng
+temperature.cpp
+Jan 19, 2020
+temperature conversion program using 
+command line arguments for hw00
+*/
+
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -11,31 +20,32 @@ using std::string;
 using std::vector;
 
 
+// converts F to C using C++ istringstream
 double cpp_ftoc(const char* str)
 {
 	double result;
 	std::istringstream istr(str);
 	istr >> result;
 	if (!istr) {
-		cout << "ERROR: Could not read temperature value" << endl;
 		return -9999;
 	}
 	return (result - 32.0) * (5.0 / 9.0);
 }
 
 
+// converts C to F using C strtod function
 double c_ctof(const char* str)
 {
 	char* err;
 	double result = strtod(str, &err);
 	if (*err != 0) {
-		cout << "ERROR: Could not read temperature value" << endl;
 		return -9999;
 	}
 	return result * (9.0 / 5.0) + 32.0;
 }
 
 
+// prints out usage help
 void printHelp()
 {
 	cout << "Available functions: --ftoc, --ctof\n"
@@ -45,17 +55,24 @@ void printHelp()
 
 int main(int argc, char** argv)
 {
+	// checks if there are enough arguments
 	if (argc >= 3) {
+
+		// --ftoc argument given
 		if (argv[1] == string("--ftoc")) {
 			double result = cpp_ftoc(argv[2]);
 			if (result == -9999) {
+				cout << "ERROR: Could not read temperature value" << endl;
 				return 0;
 			}
 			cout << result << endl;
 		}
+
+		// --ctof argument given
 		else if (argv[1] == string("--ctof")) {
 			double result = c_ctof(argv[2]);
 			if (result == -9999) {
+				cout << "ERROR: Could not read temperature value" << endl;
 				return 0;
 			}
 			cout << result << endl;
