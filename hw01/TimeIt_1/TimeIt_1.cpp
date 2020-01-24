6ab50e94363bbcdfef53d285b1afa91e4128035f
Andrew Ng <ngandrew1337@gmail.com>
2020-01-24 11:31:33 -0900

    Adding base of random vector generator

--- hw01/TimeIt_1/TimeIt_1.cpp
+++ hw01/TimeIt_1/TimeIt_1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <cmath>
 #include "Stopwatch.h"
 
 
@@ -11,8 +13,20 @@ using std::vector;
 
 int main()
 {
-	Stopwatch newTime;
-	newTime.stop();
-	newTime.reportSec();
-	newTime.reportMilli();
+	for (int i = 1; i < 5; ++i) {
+		vector<int> v(pow(10, i));
+		std::generate(v.begin(), v.end(),
+			[n = pow(10, i)]() {
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<int> dist(1, n);
+			return dist(gen);
+		});
+
+
+		for (auto n : v) {
+			cout << n << endl;
+		}
+		cout << endl;
+	}
 }
