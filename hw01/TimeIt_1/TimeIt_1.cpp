59ae4eafa207b60967cc7049faab2629407c4b56
Andrew Ng <ngandrew1337@gmail.com>
2020-01-26 17:40:37 -0900

    Cleaned vector generator, added Stopwatches for Sort and Search

--- hw01/TimeIt_1/TimeIt_1.cpp
+++ hw01/TimeIt_1/TimeIt_1.cpp
@@ -14,19 +14,42 @@ using std::vector;
 int main()
 {
 	for (int i = 1; i < 5; ++i) {
-		vector<int> v(pow(10, i));
-		std::generate(v.begin(), v.end(),
-			[n = pow(10, i)]() {
-			std::random_device rd;
-			std::mt19937 gen(rd());
-			std::uniform_int_distribution<int> dist(1, n);
-			return dist(gen);
-		});
+		int size = pow(10, i);
+
+		vector<int> orig(size);
+
+		std::generate(orig.begin(), orig.end(),
+			[size]() {
+				std::random_device rd;
+				std::mt19937 gen(rd());
+				std::uniform_int_distribution<int> dist(1, size);
+				return dist(gen);
+			});
+
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<int> dist(1, size);
+
+		cout << "Vector Size: " << size << "\n";
+
+		cout << "Algorithm: Sort\n";
+		Stopwatch sortTime;
+		std::sort(orig.begin(), orig.end());
+		sortTime.stop();
+		sortTime.reportSec();
+		sortTime.reportMilli();
+
+		cout << "Algorithm: Search\n";
+
+		auto searchIter = orig.begin() + dist(gen) - 1;
+
+		Stopwatch searchTime;
+		std::search(orig.begin(), orig.end(), searchIter, searchIter + 1);
+		searchTime.stop();
+		searchTime.reportSec();
+		searchTime.reportMilli();
 
 
-		for (auto n : v) {
-			cout << n << endl;
-		}
-		cout << endl;
+		cout << "\n";
 	}
 }
