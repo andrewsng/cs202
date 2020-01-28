26c86660d9332e32c6e96e1ca93a81a7f1767da6
Andrew Ng <ngandrew1337@gmail.com>
2020-01-27 23:45:10 -0900

    Implemented find algorithm for random string

--- hw01/TimeIt_2/TimeIt_2.cpp
+++ hw01/TimeIt_2/TimeIt_2.cpp
@@ -49,17 +49,52 @@ int main()
 		}
 	}
 
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(1, bookVec.size());
+
+	int index = dist(gen) - 1;
+	string randStr = bookVec[index];
+	
+	cout << "Algorithm: Sort (list)\n";
 	bookList.sort();
 
+	cout << "Algorithm: Find (list)\n";
+	auto listIter = std::find(bookList.begin(), bookList.end(), randStr);
+
+	cout << "Algorithm: Sort (vector)\n";
 	std::sort(bookVec.begin(), bookVec.end());
 
+	cout << "Algorithm: Find (vector)\n";
+	auto vecIter = std::find(bookVec.begin(), bookVec.end(), randStr);
+
+	cout << "Algorithm: Sort (map)\n";
 	Stopwatch mapSort;
 	map<string, unsigned int, std::less<string>> bookMapS(bookMap.begin(), bookMap.end());
 	mapSort.stop();
 	mapSort.reportSec();
 	mapSort.reportMilli();
 
+	cout << "Algorithm: Find (map)\n";
+	auto mapIter = bookMap.find(randStr);
+
+
+	int listIndex = std::distance(bookList.begin(), listIter);
+	int vecIndex = vecIter - bookVec.begin();
+	int mapIndex = std::distance(bookMap.begin(), mapIter);
+
+	cout << "\n";
+	cout << randStr << "\n";
+	cout << "List found: " << *listIter << listIndex << "\n";
+	cout << "Vector found: " << *vecIter << vecIndex << "\n";
+	cout << "Map found: " << mapIter->first << mapIndex << "\n";
+	cout << bookList.size() << "\n";
+	cout << bookVec.size() << "\n";
+	cout << bookMap.size() << "\n";
+
+	/*
 	for (auto w : bookMapS) {
 		cout << w.first << " ";
 	}
+	*/
 }
