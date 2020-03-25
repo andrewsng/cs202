31f65188d890dcf8f957333183bd709211988ca7
Andrew Ng <ngandrew1337@gmail.com>
2020-03-24 16:26:07 -0800

    Print additional labels

--- Labs/Lab007/main.cpp
+++ Labs/Lab007/main.cpp
@@ -12,18 +12,21 @@
 
 int main()
 {
+	std::cout << "1. cout: Default constructor\n";
 	Vec3 myVec;
-	std::cout << myVec << "\n";
+	std::cout << myVec << "\n\n";
 
+	std::cout << "2. cout: After setters\n";
 	Vec3 myVec2;
 	myVec2.setX(0.1234);
 	myVec2.setY(-5.4321);
 	myVec2.setZ(7.8910);
-	std::cout << myVec2 << "\n";
+	std::cout << myVec2 << "\n\n";
 
+	std::cout << "3. cout: After assignment\n";
 	Vec3 myVec3;
 	myVec3 = myVec2;
-	std::cout << myVec3 << "\n";
+	std::cout << myVec3 << "\n\n";
 
 	return 0;
 }
