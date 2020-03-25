5cf8de7afea5189c7ef471ff75bb9e931ec029b9
Andrew Ng <ngandrew1337@gmail.com>
2020-03-24 16:01:21 -0800

    Adding files, writing class

---
+++ Labs/Lab007/Vec3.h
@@ -0,0 +1,16 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+
+class Vec3 {
+public:
+	Vec3();
+
+
+private:
+	double x_{ 1.0 };
+	double y_{ 1.0 };
+	double z_{ 1.0 };
+};
+
+#endif
---
+++ Labs/Lab007/main.cpp
@@ -0,0 +1,17 @@
+/*
+ main.cpp
+ Andrew Ng
+ Mar 24, 2020
+ main for lab007 for cs202
+*/
+
+#include <iostream>
+#include "Vec3.h"
+
+
+int main()
+{
+	Vec3 myVec;
+
+	return 0;
+}
