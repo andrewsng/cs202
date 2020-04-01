0263e09e17e90e42d7c687ea6a9b269159a3f6a2
Andrew Ng <ngandrew1337@gmail.com>
2020-03-31 18:20:39 -0800

    implement Stopwatch class

--- iditarod_1_2/Recursion_2/recursion2.cpp
+++ iditarod_1_2/Recursion_2/recursion2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Stopwatch.h"
 
 
 int ack(int m, int n)
@@ -18,15 +19,19 @@ int ack(int m, int n)
 int main()
 {
 	for (int m = 0; m < 5; m++) {
-		for (int n = 0; n < 100; n++) {
-			if (m == 3 && n == 12) {
-				break;
-			}
+		for (int n = 0; n < 12; n++) {
 			if (m == 4 && n == 1) {
 				break;
 			}
+
+			int result;
+			Stopwatch time;
+			result = ack(m, n);
+			time.stop();
+
 			std::cout << "A(" << m << ", " << n;
-			std::cout << ") = " << ack(m, n) << "\n";
+			std::cout << ") = " << result << "\n";
+			time.reportMilli();
 		}
 	}
 }
