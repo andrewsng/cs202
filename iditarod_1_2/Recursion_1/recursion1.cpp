78631cb1df617b8e12a49ff5bbfa20e7ace29c6b
Andrew Ng <ngandrew1337@gmail.com>
2020-03-31 17:19:55 -0800

    write fib_loop(n)

--- iditarod_1_2/Recursion_1/recursion1.cpp
+++ iditarod_1_2/Recursion_1/recursion1.cpp
@@ -6,12 +6,29 @@ unsigned long long fib(int n)
 	if (n < 2) {
 		return n;
 	}
-
 	return fib(n - 1) + fib(n - 2);
+}
+
+
+unsigned long long fib_loop(int n)
+{
+	if (n < 2) {
+		return n;
+	}
+	unsigned long long result = 0;
+	unsigned long long prev2 = 0;
+	unsigned long long prev1 = 1;
+	for (int i = 1; i < n; i++) {
+		result = (prev1 + prev2);
+		prev2 = prev1;
+		prev1 = result;
+	}
+	return result;
 }
 
 
 int main()
 {
-	std::cout << fib(50) << "\n";
+	//std::cout << fib(50) << "\n";
+	std::cout << fib_loop(50) << "\n";
 }
