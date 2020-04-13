8592b16a50454757e3123919f878da756ffa0188
Andrew Ng <ngandrew1337@gmail.com>
2020-04-13 14:42:49 -0800

    Lab11: A default derived class object

--- Labs/Lab011/lab011.cpp
+++ Labs/Lab011/lab011.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 
 class Base
@@ -6,7 +7,7 @@ class Base
 public:
     Base() : num_(123)
     {
-        std::cout << "Constructing a default base object with value: " << num_ << "\n";
+        std::cout << "Constructing a default base object\n";
     }
 
     Base(int num) : num_(num)
@@ -25,11 +26,36 @@ private:
 };
 
 
+class Derived : public Base
+{
+public:
+    Derived() : word_("howdy")
+    {
+        std::cout << "Constructing a default derived object\n";
+    }
+
+    ~Derived()
+    {
+        std::cout << "Destructing a derived object with word: " << word_ << "\n";
+    }
+
+
+private:
+    std::string word_;
+
+};
+
+
 int main()
 {
     { Base myBase; }
     std::cout << "\n";
+
     { Base myBase(456); }
+    std::cout << "\n";
+
+    { Derived myDerived; }
+    std::cout << "\n";
 
     return 0;
 }
