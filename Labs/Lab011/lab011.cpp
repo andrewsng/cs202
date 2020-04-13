4cad60b32be0dd801055179b8685393d9b073ddd
Andrew Ng <ngandrew1337@gmail.com>
2020-04-13 14:28:05 -0800

    Lab11: A base class object w/ 1 param

--- Labs/Lab011/lab011.cpp
+++ Labs/Lab011/lab011.cpp
@@ -4,11 +4,15 @@
 class Base
 {
 public:
-    Base() : num_( 123 )
+    Base() : num_(123)
     {
         std::cout << "Constructing a default base object with value: " << num_ << "\n";
     }
 
+    Base(int num) : num_(num)
+    {
+        std::cout << "Constructing a base object with param value: " << num_ << "\n";
+    }
 
     ~Base() {
         std::cout << "Destructing a base object with value: " << num_ << "\n";
@@ -24,6 +28,8 @@ private:
 int main()
 {
     { Base myBase; }
+    std::cout << "\n";
+    { Base myBase(456); }
 
     return 0;
 }
