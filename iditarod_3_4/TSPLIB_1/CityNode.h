b6e49ed4411d678c931f75ed42f90a32f0212bd6
Andrew Ng <ngandrew1337@gmail.com>
2020-04-15 01:22:46 -0800

    Write CityNode class

---
+++ iditarod_3_4/TSPLIB_1/CityNode.h
@@ -0,0 +1,28 @@
+#ifndef CITYNODE_H
+#define CITYNODE_H
+
+
+class CityNode
+{
+public:
+
+    CityNode()
+    {}
+
+    CityNode(int num, double latitude,
+        double longitude, double x, double y)
+        : number_(num), latitude_(latitude),
+          longitude_(longitude), x_(x), y_(y)
+    {}
+
+
+private:
+
+    unsigned int number_{ 0 };
+    double latitude_{ -1.0 };
+    double longitude_{ -1.0 };
+    double x_{ -1.0 };
+    double y_{ -1.0 };
+};
+
+#endif
---
+++ iditarod_3_4/TSPLIB_1/main.cpp
@@ -0,0 +1,16 @@
+/*
+    main.cpp
+    Andrew Ng
+    Apr 14, 2020
+    main for TSPLIB part 1
+*/
+
+
+#include <iostream>
+#include "CityNode.h"
+
+
+int main()
+{
+    
+}
