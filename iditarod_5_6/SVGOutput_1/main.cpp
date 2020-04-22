ffe967e088e0d8f681ef4663ba067d2b27387c1c
Andrew Ng <ngandrew1337@gmail.com>
2020-04-22 01:37:30 -0800

    produce charts for randomly solved tours

--- iditarod_5_6/SVGOutput_1/main.cpp
+++ iditarod_5_6/SVGOutput_1/main.cpp
@@ -14,30 +14,23 @@
 
 int main()
 {
-	std::cout << "---SolveRandomly---\n";
-    TspSolver random;
-	Stopwatch randomSW;
-    random.SolveRandomly("usa13509.tsp", 1);
-	randomSW.stop();
-	randomSW.reportSec();
-	std::cout << "\n";
-	random.outputSVG("usa13509Rand.svg", 1700, 1000);
-    // usa13509 best: 2.11143e+09
+    TspSolver random1;
+    random1.SolveRandomly("dsj1000.tsp", 1);
+	random1.outputSVG("dsj1000Rand.svg", 1700, 1000);
 
-	std::cout << "---SolveGreedy (Nearest Neighbor)---\n";
-    TspSolver greedy;
-	Stopwatch greedySW;
-    greedy.SolveGreedy("usa13509.tsp");
-	greedySW.stop();
-	greedySW.reportSec();
-	std::cout << "\n";
-	greedy.outputSVG("usa13509Greedy.svg", 1700, 1000);
+	TspSolver random2;
+	random2.SolveRandomly("u1432.tsp", 1);
+	random2.outputSVG("u1432Rand.svg", 1700, 1000);
 
-	/*std::cout << "---SolveMyWay (Nearest Insertion)---\n";
-	TspSolver myWay;
-	Stopwatch myWaySW;
-	myWay.SolveMyWay("dsj1000.tsp");
-	myWaySW.stop();
-	myWaySW.reportSec();
-	std::cout << "\n";*/
+	TspSolver random3;
+	random3.SolveRandomly("fnl4461.tsp", 1);
+	random3.outputSVG("fnl4451Rand.svg", 1700, 1000);
+
+	TspSolver random4;
+	random4.SolveRandomly("rl11849.tsp", 1);
+	random4.outputSVG("rl11849Rand.svg", 1700, 1000);
+
+	TspSolver random5;
+	random5.SolveRandomly("usa13509.tsp", 1);
+	random5.outputSVG("usa13509Rand.svg", 1700, 1000);
 }
