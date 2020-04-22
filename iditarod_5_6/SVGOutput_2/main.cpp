e2d870a70ff2dda77507aed978b664496f648e34
Andrew Ng <ngandrew1337@gmail.com>
2020-04-22 04:25:48 -0800

    output 16 WIP SVGs for custom

--- iditarod_5_6/SVGOutput_2/main.cpp
+++ iditarod_5_6/SVGOutput_2/main.cpp
@@ -1,8 +1,8 @@
 /*
     main.cpp
     Andrew Ng
-    Apr 14, 2020
-    main for TSPLIB part 2
+    Apr 22, 2020
+    main for SVG Output part 2
 */
 
 
@@ -14,15 +14,12 @@
 
 int main()
 {
-	/*TspSolver random;
+	TspSolver random;
 	random.SolveRandomly("fnl4461.tsp", 1);
-	//random3.outputSVG("fnl4451Rand.svg", 1000, 1000);*/
 
 	TspSolver greedy;
 	greedy.SolveGreedy("fnl4461.tsp");
-	//greedy3.outputSVG("fnl4451Greedy.svg", 1000, 1000);
 
-	/*TspSolver custom;
+	TspSolver custom;
 	custom.SolveMyWay("fnl4461.tsp");
-	//custom3.outputSVG("fnl4451Custom.svg", 1000, 1000);*/
 }
