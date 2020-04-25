162fe80ab0d7d1419ee17a8d49751da81b2e889c
Andrew Ng <ngandrew1337@gmail.com>
2020-04-24 21:50:21 -0800

    Write twice function, demonstrate int/double/str

---
+++ Labs/Lab014/Lab014.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+template <typename T>
+T twice(const T& x)
+{
+    return x + x;
+}
+
+
+int main(int argc, char** argv)
+{
+    std::cout << twice(2) << std::endl;
+    std::cout << twice(2.3) << std::endl;
+    std::cout << twice(std::string("Hello")) << std::endl;
+
+    return 0;
+}
